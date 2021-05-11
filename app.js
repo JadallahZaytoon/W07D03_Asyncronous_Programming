cf68a8a457e5340e65b9b4b57927cf85d34268ca
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-12 00:59:35 +0300

    pulse Check solved

--- app.js
+++ app.js
@@ -16,11 +16,53 @@ const readFile = () => {
     })
   };
 
-  readFile()
+  readFile();
 
-  app.get('/', (req,res) => {
-      res.send('hello')
-  })
+//   app.get('/', (req,res) => {
+//       res.send('hello')
+//   })
+
+const writeFile = () => {
+    let waitingData=`A new file has been created`
+    fs.writeFile(`./data.txt`,waitingData,(err)=>{
+
+        if (err) {
+            console.log(err);
+          }
+    });
+  };
+
+  writeFile();
+
+
+
+//   const getPost = (id) => {
+//     axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+//     .then((response) => {
+//         console.log(response.data);
+//       })
+
+//       .catch((err) => {
+//         throw err;
+//       })
+//   };
+  
+//   getPost(1);
+//   getPost(50);
+
+  const getPost = async(id) => {
+    await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => {
+        console.log(response.data);
+      })
+
+      .catch((err) => {
+        throw err;
+      })
+  };
+  
+  getPost(1);
+  getPost(50);
 
   app.listen(PORT, () => {
     console.log('SERVER IS WORKING ON http://localhost:' + PORT);
