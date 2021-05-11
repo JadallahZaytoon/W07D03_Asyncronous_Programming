42fdfd95fea2f47aa81088274d7c0f662edabaac
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-12 00:30:16 +0300

    Q1 solved

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const fs = require("fs");
 const axios = require("axios");
+const express = require("express");
+const app=express();
+const PORT=3000;
+
+const readFile = () => {
+    
+    fs.readFile(`./data.txt`,(err,data)=>{
+        if(err){
+            throw err;
+        }
+
+        console.log(data.toString());
+
+    })
+  };
+
+  readFile()
+
+  app.get('/', (req,res) => {
+      res.send('hello')
+  })
+
+  app.listen(PORT, () => {
+    console.log('SERVER IS WORKING ON http://localhost:' + PORT);
+  });
